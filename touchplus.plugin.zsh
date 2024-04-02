2c028f92ba20db1f9519acf3483b4e4ed7140e46
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-04-03 00:07:59 +0530

    fix: supports args

--- touchplus.plugin.zsh
+++ touchplus.plugin.zsh
@@ -3,14 +3,17 @@
 
 # Override 'touch' command
 touch() {
-  # Extract the directory part of the path
-  local dir_path=$(dirname "$1")
+  # Loop through all arguments
+  for arg in "$@"; do
+    # Extract the directory part of the path
+    local dir_path=$(dirname "$arg")
 
-  # Create the directory if it doesn't exist
-  if [ ! -d "$dir_path" ]; then
-    mkdir -p "$dir_path"
-  fi
+    # Create the directory if it doesn't exist
+    if [ ! -d "$dir_path" ]; then
+      mkdir -p "$dir_path"
+    fi
 
-  # Use the original 'touch' command to create the file
-  command touch "$1"
+    # Use the original 'touch' command to create the file
+    command touch "$arg"
+  done
 }
